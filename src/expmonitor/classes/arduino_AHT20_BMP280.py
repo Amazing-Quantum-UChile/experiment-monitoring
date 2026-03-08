a1e06ba8d66aea4e5898f445f2aa3f94c44940a1
Victor <victor.gondret@posteo.net>
2026-03-08 16:28:20 -0300

    [AHT20_BMP280] Imlementing the pressure sensor

---
+++ src/expmonitor/classes/arduino_AHT20_BMP280.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+# -*- mode:Python; coding: utf-8 -*-
+
+# ----------------------------------
+# Created on the Sun Mar 08 2026 by Victor
+#
+# Copyright (c) 2026 - AmazingQuantum@UChile
+# ----------------------------------
+#
+'''
+Content of arduino_AHT20_BMP280.py
+
+Implements the AHT20+BMP280 sensor to dialog with the arduino.
+--> When queried with "p", the Arduino returns a string with the temperature, humidity and pressure values separated by a comma, e.g. "24.5, 45.2, 1013.25".
+See the arduino file in arduino_src/main to access the Arduino driver. 
+'''
+
+from expmonitor.classes.arduino import ArduinoMultiSensor
+import random
+
+class Arduino_AHT20_BMP280_Sensor(ArduinoMultiSensor):
+    def __init__(self,
+                board, 
+                database,
+                number_of_sensors=3, 
+                descr="aht20_bmp280",
+                sensor_parameters=[{
+            "sensor_number": 0,
+            "descr": "temp_aht20_bmp280",
+            "unit": "°C",
+            "category": "temperature",
+            "sensor_type": "P-N junction temperature sensor",
+            "save_to_database": True,
+            },
+            {
+            "sensor_number": 1,
+            "descr": "humid_aht20_bmp280",
+            "unit": "%",
+            "category": "humidity",
+            "sensor_type": "capacitive polymer hygrometer",
+            "save_to_database": True,
+            "value_limit":(0,100),
+            },
+            {
+            "sensor_number": 2,
+            "descr": "pressure_aht20_bmp280",
+            "unit": "Pa",
+            "category": "pressure",
+            "sensor_type": "Piezoresistive Barometric Pressure Sensor",
+            "save_to_database": True,
+            "value_limit":(0,5000),
+            }],
+                 **kwargs):
+        super().__init__(board=board, 
+                         database=database, 
+                         number_of_sensors=number_of_sensors,
+                         descr=descr,
+                         sensor_parameters=sensor_parameters,
+                           **kwargs)
+
+    def connect(self):
+        """Open the connection to the sensor: we do nothing because connection already exists and is handle by the board object."""
+        pass
+
+    def disconnect(self):
+        """Close the connection to the sensor. We do nothing because connection already exists and is handle by the board object."""
+        pass
+
+    def rcv_vals(self):
+        """Receive and return measurement values from sensor."""
+        if not self.is_dummy:
+            result = self.board.query("p", timeout=.5)
+            try:
+                ## transform the string into list
+                return [float(elem) for elem in result.split(",")]
+            except:
+                return result
+        else:
+            return [random.random() for i in range(self.number_of_sensors)]
+
+
+
+
+
+
+
+
+
+
+
--- src/expmonitor/classes/arduino_QMC5883L.py
+++ src/expmonitor/classes/arduino_QMC5883L.py
@@ -11,49 +11,54 @@
 Content of arduino_QMC5883L.py
 
 Implements the QMC5883L sensor to dialog with the arduino.
---> When queried with "b", the Arduino returns a string with the magnetic field value along the 3 axes "X,Y,Z" e.g. "312.3,4.1,1547.8".
+--> When queried with "b", the Arduino returns a string with the magnetic field value along the 3 axes "X,Y,Z" e.g. "312,4,1547". Note that the sensor returns the result in bit unit and the conversion to Gauss depends on the hardware resolution chosen (1, 3 or 8 Gauss). To take this into account, use the calibration function conversion_fctn. 
 See the arduino file in arduino_src/main to access the Arduino driver. 
 '''
 
+from expmonitor.classes.arduino import ArduinoMultiSensor
+import random
 
-class Arduino_AHT10_Sensor(ArduinoMultiSensor):
+class Arduino_QMC5883L_Sensor(ArduinoMultiSensor):
     def __init__(self,
                 board, 
                 database,
                 number_of_sensors=3, 
                 descr="qmc5883l",
                 sensor_parameters=[{
-            "sensor_number": 0,
-            "descr": "mag_x_qmc5883l",
-            "unit": "mG",
-            "category": "magnetic field",
-            "sensor_type": "Anisotropic Magneto-Resistive sensor",
-            "save_to_database": True,
-            },
-            {
-            "sensor_number": 1,
-            "descr": "mag_y_qmc5883l",
-            "unit": "mG",
-            "category": "magnetic field",
-            "sensor_type": "Anisotropic Magneto-Resistive sensor",
-            "save_to_database": True,
-            },
-            {
-            "sensor_number": 2,
-            "descr": "mag_z_qmc5883l",
-            "unit": "mG",
-            "category": "magnetic field",
-            "sensor_type": "Anisotropic Magneto-Resistive sensor",
-            "save_to_database": True,
-            },],
+                "sensor_number": 0,
+                "descr": "mag_x_qmc5883l",
+                "unit": "mG",
+                "category": "magnetic field",
+                "sensor_type": "Anisotropic Magneto-Resistive sensor",
+                "conversion_fctn":lambda t: t,
+                "save_to_database": True,
+                },
+                {
+                "sensor_number": 1,
+                "descr": "mag_y_qmc5883l",
+                "unit": "mG",
+                "category": "magnetic field",
+                "sensor_type": "Anisotropic Magneto-Resistive sensor",
+                "conversion_fctn":lambda t: t,
+                "save_to_database": True,
+                },
+                {
+                "sensor_number": 2,
+                "descr": "mag_z_qmc5883l",
+                "unit": "mG",
+                "category": "magnetic field",
+                "sensor_type": "Anisotropic Magneto-Resistive sensor",
+                "conversion_fctn":lambda t: t,
+                "save_to_database": True,
+                },],
                  **kwargs):
 
         super().__init__(board=board, 
-                            database=database, 
-                            number_of_sensors=number_of_sensors,
-                            descr=descr,
-                            sensor_parameters=sensor_parameters,
-                            **kwargs)
+                        database=database, 
+                        number_of_sensors=number_of_sensors,
+                        descr=descr,
+                        sensor_parameters=sensor_parameters,
+                        **kwargs)
 
     def connect(self):
         """Open the connection to the sensor: we do nothing because connection already exists and is handle by the board object."""
